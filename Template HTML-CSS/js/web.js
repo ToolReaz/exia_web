843c8e20385bdb7e0976f13195b2a4c64cf5b710
Flingouin <matthieu.fritsch@viacesi.fr>
2018-04-09 16:37:41 +0200

    Index

--- Template HTML-CSS/js/web.js
+++ Template HTML-CSS/js/web.js
@@ -1,5 +1,8 @@
 var slideIndex = 1;
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
 showSlides(slideIndex);
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -12,15 +15,33 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+function plusSlides2(n2) {
+  showSlides2(slideIndex2 += n2);
+}
+
+function currentSlide2(n2) {
+  showSlides2(slideIndex2 = n2);
+}
+
+function showSlides2(n2) {
+  var i2;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  if (n2 > slides2.length) {slideIndex2 = 1}
+  if (n2 < 1) {slideIndex2 = slides2.length}
+  for (i2 = 0; i2 < slides2.length; i2++) {
+      slides2[i2].style.display = "none";
+  }
+  slides2[slideIndex2-1].style.display = "block";
 }
