2d6d616d2ea6fff7c95dfa3f9c73e05dbdb36ead
Thomas <toolreaz@outlook.fr>
2018-04-11 08:53:15 +0200

    Fix

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Route, Switch} from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 import UserRegister from "./components/user/UserRegister";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
@@ -11,16 +12,18 @@ import IdeasPage from "./components/IdeasPage";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Nav />
-          <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/user/register" component={UserRegister} />
-              <Route path="/user/connect" component={UserConnect} />
-              <Route path="/admin" component={Admin} />
-              <Route path="/idee" component={IdeasPage} />
-          </Switch>
-      </div>
+        <CookiesProvider>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/user/register" component={UserRegister} />
+                    <Route path="/user/connect" component={UserConnect} />
+                    <Route path="/admin" component={Admin} />
+                    <Route path="/idee" component={IdeasPage} />
+                </Switch>
+            </div>
+        </CookiesProvider>
     );
   }
 }
--- src/server/api/controller/IdeaController.js
+++ src/server/api/controller/IdeaController.js
@@ -11,5 +11,15 @@ module.exports = {
 
     create: (req, res) => {
         let reqName = req.body.name;
+        let reqText = req.body.text;
+        let reqToken = req.body.token;
+
+        if (!reqName || !reqText || !reqToken) {
+            res.json({'error': 'Champs incorrect !'});
+        } else {
+            DB.GetTokenTime(reqToken, (callback) =>  {
+                console.log(callback);
+            })
+        }
     }
 };
