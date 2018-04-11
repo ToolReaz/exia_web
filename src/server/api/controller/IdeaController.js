0b712b07f6f4f880666895eb62c6894c083664ea
Thomas <toolreaz@outlook.fr>
2018-04-11 14:05:10 +0200

    Implemented all controller's methods

--- src/server/api/controller/IdeaController.js
+++ src/server/api/controller/IdeaController.js
@@ -3,27 +3,35 @@ const DB = require('../model/DB');
 module.exports = {
 
     getAll: (req, res) => {
-        DB.Idea.GetAllIdeas((result) => {
-            res.json({'error': null, 'ideas': result});
-        });
-        //res.json({'error': 'Impossible de récupérer les données sur le server !'});
+        DB.Idea.GetAllIdeas().then((ideas) => {
+            res.json({'error': null, 'content': ideas.dataValues});
+        }).catch((reason) => res.json({'error': 'Impossible de récupérer les idées !', 'content': null}));
     },
 
     create: (req, res) => {
         let reqName = req.body.name;
         let reqText = req.body.text;
-        let reqToken = req.body.token;
+        let reqToken = req.cookies.token;
 
         if (!reqName || !reqText || !reqToken) {
             res.json({'error': 'Champs incorrect !'});
         } else {
-            DB.Token.GetTokenTime(reqToken, (callback) =>  {
-                console.log(callback);
-            })
+            DB.Token.GetAccountFromToken(reqToken).then((id) => {
+                DB.Idea.CreateIdea(id, reqName, reqText, null).then((success) => {
+                    res.json({'error': null, 'content': null});
+                })
+            }).catch((reason) => res.json({'error': reason, 'content': null}));
         }
     },
 
     vote: (req, res) => {
+        let reqVoteId = req.params.id;
+        let reqToken = req.cookies.token;
 
+        DB.Token.GetAccountFromToken(reqToken).then((id) => {
+            DB.Idea.VoteIdea(id, reqVoteId, true).then(() => {
+                res.json({'error': null, 'content': null});
+            }).catch((reason => res.json({'error': reason, 'content': null}));
+        })
     }
 };
