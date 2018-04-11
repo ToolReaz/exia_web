9862f1d5146a5007348e71c031ee938823505899
Thomas <toolreaz@outlook.fr>
2018-04-11 15:32:00 +0200

    Created personal css for dev

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Route, Switch} from "react-router-dom";
-import { CookiesProvider } from 'react-cookie';
 import UserRegister from "./components/user/UserRegister";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import UserConnect from "./components/user/UserConnect";
 import Admin from "./pages/admin";
-import IdeasPage from "./components/IdeasPage";
+import IdeasPage from "./pages/user/IdeasPage";
 import Index from "./pages";
 
 class App extends Component {
   render() {
     return (
-        <CookiesProvider>
-            <div className="App">
-                <Nav />
-                <Switch>
-                    <Route exact path="/" component={Index} />
-                    <Route path="/user/register" component={UserRegister} />
-                    <Route path="/user/connect" component={UserConnect} />
-                    <Route path="/admin" component={Admin} />
-                    <Route path="/idee" component={IdeasPage} />
-                </Switch>
-                <Footer />
-            </div>
-        </CookiesProvider>
+        <div className="App">
+            <Nav />
+            <Switch>
+                <Route exact path="/" component={Index} />
+                <Route path="/user/register" component={UserRegister} />
+                <Route path="/user/connect" component={UserConnect} />
+                <Route path="/admin" component={Admin} />
+                <Route path="/idee" component={IdeasPage} />
+            </Switch>
+            <Footer />
+        </div>
     );
   }
 }
--- src/client/src/components/Nav.js
+++ src/client/src/components/Nav.js
@@ -3,36 +3,62 @@ import {Link} from "react-router-dom";
 
 class Nav extends Component {
 
+    static openNav() {
+        var menu = document.getElementById("header-nav-container");
+        if (menu.style.display === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    }
+
     render() {
         return (
-            <div>
-                <nav>
-                    <div className="nav-item">
-                        <Link to="/">Accueil</Link>
+            <header>
+                <div className="header-content">
+                    <div className="header-hamburger" onClick={Nav.openNav}>
+                        <img src="btn-hamburger.png" alt=""/>
                     </div>
-                    <div className="nav-item">
-                        <Link to="/">L'équipe</Link>
+                    <div className="header-search desktop-only">
+                        <input type="text" placeholder="Chercher"/>
                     </div>
-                    <div className="nav-item">
-                        <Link to="/">Evènements</Link>
+                    <div className="header-logo">
+                        <img src="logobde.png" alt="logo" draggable="false"/>
                     </div>
-                    <div className="nav-item">
-                        <Link to="/">Boutique</Link>
-                    </div>
-                    <div className="nav-item">
+                    <div className="header-account desktop-only">
                         <Link to="/user/register">Inscription</Link>
-                    </div>
-                    <div className="nav-item">
                         <Link to="/user/connect">Connexion</Link>
                     </div>
-                    <div className="nav-item">
-                        <Link to="/admin">Admin</Link>
-                    </div>
-                    <div className="nav-item">2
-                        <Link to="/idee">Idee</Link>
-                    </div>
-                </nav>
-            </div>
+                </div>
+                <div id="header-nav-container" className='header-nav-container'>
+                    <nav>
+                        <div className="nav-item">
+                            <Link to="/">Accueil</Link>
+                        </div>
+                        <div className="nav-item">
+                            <Link to="/">L'équipe</Link>
+                        </div>
+                        <div className="nav-item">
+                            <Link to="/">Evènements</Link>
+                        </div>
+                        <div className="nav-item">
+                            <Link to="/">Boutique</Link>
+                        </div>
+                        <div className="nav-item">
+                            <Link to="/admin">Admin</Link>
+                        </div>
+                        <div className="nav-item">
+                            <Link to="/idee">Idee</Link>
+                        </div>
+                        <div className="nav-item mobile-only">
+                            <Link to="/user/register">Inscription</Link>
+                        </div>
+                        <div className="nav-item mobile-only">
+                            <Link to="/user/connect">Connexion</Link>
+                        </div>
+                    </nav>
+                </div>
+            </header>
         )
     }
 }
