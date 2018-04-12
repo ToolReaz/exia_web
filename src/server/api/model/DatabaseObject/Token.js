abf7e630e6cae967bb502d561ec6fccc3151f77c
GitHubbles <jerome.zilliox@viacesi.fr>
2018-04-12 14:19:52 +0200

    Updated doc

--- src/server/api/model/DatabaseObject/Token.js
+++ src/server/api/model/DatabaseObject/Token.js
@@ -5,6 +5,7 @@ module.exports = (dataObject, permissions) => {
         /**
          * Retourne un compte à partir d'un token (penser à vérifier la validité du token avec GetTokenTime)
          * @param {string} token Token de connexion
+         * @returns {Promise<number>}
          */
         GetAccountFromToken: (token) => {
             return new Promise((resolve, reject) => {
