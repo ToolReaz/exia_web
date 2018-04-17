a21bb6de7a6a5d65648ad2bfd3a9e54add1baac4
Thomas <toolreaz@outlook.fr>
2018-04-17 14:08:17 +0200

    removed .dataValues

--- src/server/api/controller/IdeaController.js
+++ src/server/api/controller/IdeaController.js
@@ -51,7 +51,7 @@ module.exports = {
         if (reqToken) {
             DB.Token.GetAccountFromToken(reqToken).then(id => {
                 DB.Idea.GetIdeaFromId(id, reqID).then((idea) => {
-                    res.json({'error': null, 'content': idea.dataValues});
+                    res.json({'error': null, 'content': idea});
                 }).catch((reason) => {
                     // Catch DB errors
                     res.json({'error': reason, 'content': null});
